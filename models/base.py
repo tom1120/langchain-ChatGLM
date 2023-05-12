5333c9e8ca69292e564773330acd1a83534ddb56
glide-the <2533736852@qq.com>
2023-05-12 14:55:05 +0800

    BaseAnswer的生命周期，执行过程和相关对象解释

--- models/base.py
+++ models/base.py
@@ -45,6 +45,8 @@ class AnswerResultQueueSentinelTokenListenerQueue(transformers.StoppingCriteria)
     """
      定义模型stopping_criteria 监听者，在每次响应时将队列数据同步到AnswerResult
      实现此监听器的目的是，不同模型的预测输出可能不是矢量信息，hf框架可以自定义transformers.StoppingCriteria入参来接收每次预测的Tensor和损失函数，
+     通过给 StoppingCriteriaList指定模型生成答案时停止的条件。每个 StoppingCriteria 对象表示一个停止条件
+     当每轮预测任务开始时，StoppingCriteria都会收到相同的预测结果，最终由下层实现类确认是否结束
      输出值可用于 generatorAnswer generate_with_streaming的自定义参数观测，以实现更加精细的控制
     """
 
@@ -81,9 +83,25 @@ class Iteratorize:
         def _callback(val):
             """
             模型输出预测结果收集
-            通过 给 StoppingCriteriaList指定模型生成答案时停止的条件。每个 StoppingCriteria 对象表示一个停止条件
-            每个StoppingCriteria都会收到相同的预测结果，最终由下层实现类确认是否结束
+            通过定义generate_with_callback收集器AnswerResultStream，收集模型预测的AnswerResult响应结果，最终由下层实现类确认是否结束
+            结束条件包含如下
+                1、模型预测结束、收集器self.q队列收到 self.sentinel标识
+                2、在处理迭代器队列消息时返回了break跳出迭代器，触发了StopIteration事件
+                3、模型预测出错
             因为当前类是迭代器，所以在for in 中执行了break后 __exit__ 方法会被调用，最终stop_now属性会被更新，然后抛出异常结束预测行为
+            迭代器收集的行为如下
+                创建Iteratorize迭代对象，
+                定义generate_with_callback收集器AnswerResultStream
+                启动一个线程异步预测结果来调用上游checkpoint的实现方法_generate_answer
+                _generate_answer通过generate_with_callback定义的收集器，收集上游checkpoint包装的AnswerResult消息体
+                由于self.q是阻塞模式，每次预测后会被消费后才会执行下次预测
+                这时generate_with_callback会被阻塞
+                主线程Iteratorize对象的__next__方法调用获取阻塞消息并消费
+                    1、消息为上游checkpoint包装的AnswerResult消息体，返回下游处理
+                    2、消息为self.sentinel标识，抛出StopIteration异常
+                主线程Iteratorize对象__exit__收到消息，最终stop_now属性会被更新
+                异步线程检测stop_now属性被更新，抛出异常结束预测行为
+            迭代行为结束
             :param val:
             :return:
             """
